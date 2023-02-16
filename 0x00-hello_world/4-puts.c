5a44bf83cabd85d3c986b9095ee2e1e73272f8f5
Gatjuat Wicteat Riek <gatjuatriek@gmail.com>
2023-02-16 13:50:22 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - This is a  program which print a line with puts function
+ * Return: 0(succesfull execution of code)
+ */
+int main(void)
+{
+puts(" \"programming is like building a multilingual puzzle");
+return (0);
+}
+
