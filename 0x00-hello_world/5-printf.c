42b957f1ba71da8228c1f3185e666bd6222f2b8b
Gatjuat Wicteat Riek <gatjuatriek@gmail.com>
2023-02-16 14:25:39 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A program that prints out a line with printf function
+ * Return: 0 (successfull execution of codes)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
