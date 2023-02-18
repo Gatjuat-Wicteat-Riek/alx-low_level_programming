6e1b56e544c4eca07c9c298742fa21ec80631277
Gatjuat Wicteat Riek <gatjuatriek@gmail.com>
2023-02-17 17:02:43 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - prints all the combination of single digits
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 48; m < 58; m++)
+	{
+		putchar(m);
+		if (m != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+	}
+}
