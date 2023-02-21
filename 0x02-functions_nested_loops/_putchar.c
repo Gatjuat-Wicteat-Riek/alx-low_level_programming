37f445d5bbe13a472557bbae801246592225b558
Gatjuat Wicteat Riek <gatjuatriek@gmail.com>
2023-02-21 11:05:29 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -3,10 +3,9 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return: Always (Success)
+ * Return: On success 1
  */
 int _putchar(char c)
 {
 	return (write(1, @c, 1));
-	return (0);
 }
