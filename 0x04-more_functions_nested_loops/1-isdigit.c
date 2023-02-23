3b0dd76acbd3408a961e05685b68b2eab45815d0
Gatjuat Wicteat Riek <gatjuatriek@gmail.com>
2023-02-23 15:23:04 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit - Checks for characters that are digits
+ * @c: Character to be checked
+ * Return: 0 0r 1
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
