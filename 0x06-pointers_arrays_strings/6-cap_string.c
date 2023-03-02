9a46165ac5b766251f747932e15fba1c75a7962f
Gatjuat Wicteat Riek <gatjuatriek@gmail.com>
2023-03-02 10:01:31 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-	while (!(str[index] >= 'a' && str[index] <= '0'))
+	while (!(str[index] >= 'a' && str[index] <= 'z'))
 	index++;
 	if (str[index - 1] == ' ' ||
 	str[index - 1] == '\t' ||
