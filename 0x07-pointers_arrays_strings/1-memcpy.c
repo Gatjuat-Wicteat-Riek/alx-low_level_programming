888408b537e7375c66ac334c1babf8ef0b978fa6
Gatjuat Wicteat Riek <gatjuatriek@gmail.com>
2023-03-07 09:07:09 -0800

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy - a function that copies memory areA
+ * @dest: The destination where the file will be keep
+ * @src: the source of the file
+ * @n: The number of bytes
+ * Return: coppies of memory files
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = 0;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+
+	return (dest);
+}
